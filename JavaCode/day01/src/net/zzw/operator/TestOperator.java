3727fe64bc2618088ccf50bce37a9175a4c727bb
zzw <dgzzw1@163.com>
2021-12-26 15:39:51 +0800

    三元运算符

---
+++ JavaCode/day01/src/net/zzw/operator/TestOperator.java
@@ -0,0 +1,16 @@
+package net.zzw.operator;
+
+/**
+ * 三元运算符
+ *
+ * @author zzw dgzzw1@163.com
+ * @version 2021/12/26 15:38
+ * @since JDK11
+ */
+public class TestOperator {
+	public static void main(String[] args) {
+		int score = 90;
+		String str = score < 60 ? "不及格" : "合格";
+		System.out.println(str);
+	}
+}
