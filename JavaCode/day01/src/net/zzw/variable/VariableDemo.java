233fe99dd0667eddcbaead06e7363b7d6539d724
zzw <dgzzw1@163.com>
2021-12-26 14:43:48 +0800

    Java变量

---
+++ JavaCode/day01/src/net/zzw/variable/VariableDemo.java
@@ -0,0 +1,18 @@
+package net.zzw.variable;
+
+/**
+ * 变量使用
+ *
+ * @author zzw dgzzw1@163.com
+ * @version 2021/12/26 14:29
+ * @since JDK11
+ */
+public class VariableDemo {
+	public static void main(String[] args) {
+		// 测试变量的声明和初始化
+		int a;
+		a = 1;
+		int b = a + 3;
+		System.out.println(b);
+	}
+}
